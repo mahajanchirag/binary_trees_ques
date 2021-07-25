e7c01506c8414001819ab9050ab435021042bc15
mahajanchirag <70029905+mahajanchirag@users.noreply.github.com>
2021-07-25 20:33:07 +0530

    Create Flatten Binary Tree to Linked List.cpp

---
+++ Flatten Binary Tree to Linked List.cpp
@@ -0,0 +1,9 @@
+if (!root) return;
+    flatten(root->left);
+    flatten(root->right);
+    TreeNode *tmp = root->right;
+    root->right = root->left;
+    root->left = nullptr;
+    while (root->right)
+        root = root->right;
+    root->right = tmp;
