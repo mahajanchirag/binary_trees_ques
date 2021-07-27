24b8d8ad71f7cc3d86833061f01883c2cd458f01
CHIRAG MAHAJAN <70029905+mahajanchirag@users.noreply.github.com>
2021-07-28 01:21:46 +0530

    Create number of unique BSTs.cpp

---
+++ number of unique BSTs.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        if(n==0 ||n==1)
+        {
+            return 1;
+        }
+        
+        vector<int> dp(n+1,0);
+        
+        dp[0]=1;
+        dp[1]=1;
+        dp[2]=2;
+        
+        for(int i=3;i<n+1;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                dp[i]=dp[i]+(dp[j-1]*dp[i-j]);
+            }
+        }
+        
+        return dp[n];
+    }
+};
